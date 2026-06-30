55a6101d59b1daf6c58af555cb783de8a3372966
Maestro <kchirag.gautam.30@gmail.com>
2026-06-30 23:45:27 +0530

    games js file made

---
+++ Games/gamesCommon.js
@@ -0,0 +1,11 @@
+function saveGameResult(gameName, score) {
+  const newRecord = {
+    game: gameName,
+    date: new Date().toLocaleDateString(),
+    score: score
+  };
+
+  let history = JSON.parse(localStorage.getItem("gameHistory")) || [];
+  history.push(newRecord);
+  localStorage.setItem("gameHistory", JSON.stringify(history));
+}
